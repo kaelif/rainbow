31978c7b85b49d8dd80cfdcf08b30e5387867a21
kaelif <flanagan.kaeli@gmail.com>
2020-05-21 01:13:02 -0700

    made 'rainbow' on page show up rainbow colored

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 replaceText(document.body)
 
-function replaceText(element){
-    if (element.hasChildNodes()) {
-        element.childNodes.forEach(replaceText)
-    } else if (element.nodeType === Text.TEXT_NODE) {
-        if (element.textContent.match(/rainbow/gi)) {
-            const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(rainbow)/gi, 
-            '<span class="rainbow">$1</span>')
-            element.replaceWith(newElement)
-        }
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText)
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if (element.textContent.match(/rainbow/gi)) {
+      const newElement = document.createElement('span')
+      newElement.innerHTML = element.textContent.replace(/(rainbow)/gi, '<span class="rainbow">$1</span>')
+      element.replaceWith(newElement)
     }
+  }
 }
